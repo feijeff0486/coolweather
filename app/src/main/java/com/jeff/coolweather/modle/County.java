585ed78fb42a45e43b13a3e6f9002c4cae537841
fei z <fei z>
2017-04-22 14:23:07 +0800

    “““新增数据库的帮助类，以及各表的实体类。”

---
+++ app/src/main/java/com/jeff/coolweather/modle/County.java
@@ -0,0 +1,45 @@
+package com.jeff.coolweather.modle;
+
+/**
+ * Created by 小太阳jeff on 2017/4/22.
+ */
+
+public class County {
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+}
